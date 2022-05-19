d353ce3f045c4ba29ab25aab07a83bf97c996cd4
virginie serrure <virginie.serrure1803@gmail.com>
2022-05-19 22:35:27 +0200

    add method destroy

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,23 +1,25 @@
 class BookmarksController < ApplicationController
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
     @bookmark = Bookmark.new(bookmark_params)
     @list = List.find(params[:list_id])
-    # @list_bookmark = List.find(params[:bookmark])
-    # @list = @list_bookmark[:list_id]
     @bookmark.list = @list
-    if @bookmark.save!
-      redirect_to @list
+    if @bookmark.save
+      redirect_to list_path(@list)
     else
       render :new
     end
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list_id)
   end
 
   private
